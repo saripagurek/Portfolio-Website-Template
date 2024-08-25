e68c22ed184274bd0b8b00c4bd27091d4f92cf97
saripagurek <saripvm@gmail.com>
2024-08-24 23:08:10 -0400

    re-organize

--- Website 3/scroll.js
+++ Website 3/scroll.js
@@ -2,6 +2,8 @@ let scroll_container = document.querySelector(".gallery");
 let back_button = document.getElementById("back_button");
 let next_button = document.getElementById("next_button");
 
+var gallery_positionInfo = scroll_container.getBoundingClientRect();
+
 scroll_container.addEventListener("wheel", (event) => {
     event.preventDefault();
     scroll_container.scrollLeft += event.deltaY;
@@ -9,7 +11,7 @@ scroll_container.addEventListener("wheel", (event) => {
 
 next_button.addEventListener("click", () => {
     scroll_container.style.scrollBehavior = "smooth";
-    scroll_container.scrollLeft += 900;
+    scroll_container.scrollLeft += gallery_positionInfo.width;
 });
 
 back_button.addEventListener("click", () => {
