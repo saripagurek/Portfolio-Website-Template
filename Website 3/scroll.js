8bac788201803725c184f89af5d2c80c401b5a32
saripagurek <saripvm@gmail.com>
2024-08-25 22:28:21 -0400

    update style

--- Website 3/scroll.js
+++ Website 3/scroll.js
@@ -4,7 +4,8 @@ let next_button = document.getElementById("next_button");
 
 var gallery_positionInfo = scroll_container.getBoundingClientRect();
 
-scroll_container.addEventListener("wheel", (event) => {
+if (screen.width > 900){
+  scroll_container.addEventListener("wheel", (event) => {
     event.preventDefault();
     scroll_container.scrollLeft += event.deltaY;
 });
@@ -17,4 +18,16 @@ next_button.addEventListener("click", () => {
 back_button.addEventListener("click", () => {
     scroll_container.style.scrollBehavior = "smooth";
     scroll_container.scrollLeft -= 900;
+});
+}
+
+function getPics() {}
+const imgs = document.querySelectorAll('.gallery img');
+const fullPage = document.querySelector('#fullpage');
+
+imgs.forEach(img => {
+  img.addEventListener('click', function() {
+    fullPage.style.backgroundImage = 'url(' + img.src + ')';
+    fullPage.style.display = 'block';
+  });
 });
